e176782bd6c94641f5c4ac844c86cec8f984ca91
Ashish Gangal <ashish.gangal1990@gmail.com>
2018-02-18 09:02:24 -0800

    kdfd :wq Initial commit

---
+++ UberClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberHelper
+{
+    public class UberClient
+    {
+        private string clientid;
+        private string clientsecret;
+        private string redirect_uri;
+        private string[] scope;
+
+        #region constructor
+        /// <summary>
+        /// Creates a new client to interact with Uber's API
+        /// </summary>
+        /// <param name="clientid"></param>
+        /// <param name="clientsecret"></param>
+        /// <param name="redirect_uri"></param>
+        /// <param name="scope"></param>
+        public UberClient(string clientid, string clientsecret, string redirect_uri, string[] scope)
+        {
+            this.clientid = clientid;
+            this.clientsecret = clientsecret;
+            this.redirect_uri = redirect_uri;
+            this.scope = scope;
+        }
+        #endregion
+
+        #region public methods
+
+        private bool UserAuth()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
